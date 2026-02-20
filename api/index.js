f31244ed8a15caa1ab5b95ad5eafc9f397672dcc
sihdhiswaran-S <sihdhiswaran@gmail.com>
2026-02-20 14:44:37 +0530

    db connented successfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 import express from 'express';
-const app = express();
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000 ");
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express(); 
+app.use(express.json());
+console.log(process.env.MONGO);
+
+mongoose.connect(process.env.MONGO).then(()=>{ 
+  console.log("Connected to MongoDB"); 
+  app.listen(3000,()=>{ 
+  console.log("Server is running on port 3000 "); 
+}
+);
+} ).catch((err)=>{ 
+  console.error("Error connecting to MongoDB", err); 
 });
 

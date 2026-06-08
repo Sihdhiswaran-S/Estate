9db5e621886f6c770e89c0582f40b6fa048aac1f
sihdhiswaran-S <sihdhiswaran@gmail.com>
2026-06-08 15:46:12 +0530

    create a sigin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,22 +1,46 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
-export const signup = async(req, res, next) => {
-    console.log(req.body);
-    console.log("signup controller");
-    const { username, email, password } = req.body;
-    const hashedPassword =  bcrypt.hashSync(password, 10); 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,  
-    });
-    try {
-    await newUser.save()
+import { errorHandle } from "../Utils/error.js";
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
+  console.log(req.body);
+  console.log("signup controller");
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-        console.error("Error registering user", error);
-        next(error);
-    } 
-}
+  } catch (error) {
+    console.error("Error registering user", error);
+    next(error);
+  }
+};
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) {
+      return next(errorHandle(404, "User not found"));
+    }
+    const isPasswordValid = bcrypt.compareSync(password, validuser.password);
+    if (!isPasswordValid) {
+      return next(errorHandle(401, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const {password:pass,...rest}=validuser._doc;
+     res
+       .cookie("access_token", token, { httpOnly: true })
+       .status(200)
+       .json({ rest, message: "User signed in successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { signin, signup } from '../controller/auth.controller.js';
 const router = express.Router();
 
 // Example route for user registration  
 router.post("/signup",signup)
+router.post("/signin",signin)
 console.log("auth route");
 export default router;

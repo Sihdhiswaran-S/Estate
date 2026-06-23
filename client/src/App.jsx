8c824398fe2e60644cd3bd4ab4b9cf898f23a9aa
sihdhiswaran-S <sihdhiswaran@gmail.com>
2026-06-23 13:41:54 +0530

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Header from './component/Header'
+
 function App() {
 
   return (

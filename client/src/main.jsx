d9759bb177fcda9f90c488243a3132879be61464
sihdhiswaran-S <sihdhiswaran@gmail.com>
2026-06-24 14:26:54 +0530

    redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from "./redux/store";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
-  
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider> 
- 
+    </PersistGate>
+  </Provider>,
 );
